03a3bdbcbea6d781edd0195fe620b32e8f46201c
rashidekbal <57672024+rashidekbal@users.noreply.github.com>
2025-01-15 02:29:05 +0530

    Update Contextprovider.jsx

--- src/store/Contextprovider.jsx
+++ src/store/Contextprovider.jsx
@@ -23,10 +23,10 @@ export default function Contextprovider({ children }) {
   let [Wingouserbethistory5min, changeWingouserbethistory5min] = useState([]);
   let [Wingouserbethistory30sec, changeWingouserbethistory30sec] = useState([]);
 function setuid(uid){
-  alert("setting" +uid)
+  
   window.sessionStorage.setItem("uid",uid);
   sessionStorage.setItem("uid",uid)
-  alert(sessionStorage.getItem("uid"));
+  
 }
 
 
